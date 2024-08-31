37fd15061492a48c87caa03ecdd2bb145adc464c
stefa <stefanboehme12@gmail.com>
2024-08-31 16:00:01 +0200

    Bevor Blogpostbearbeitung

--- frontend/src/components/ExplanationSite.jsx
+++ frontend/src/components/ExplanationSite.jsx
@@ -4,13 +4,15 @@ import '../assets/css/BlogPostPage.css'; // Optional für CSS-Styling
 
 const ExplanationSite = ({ title, author, date, content }) => {
   return (
-    <div className="blog-post-page">
-      <h1 className="blog-post-page-title">Ein Projekt</h1>
-      <div className="blog-post-page-meta">
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }} className="blog-post-page">
+      <h1 style={{ marginTop: '20px' }} className="blog-post-page-title">Projektname</h1>
+        <a>Github Link</a>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '10px' }} className="blog-post-page-meta">
         <span className="blog-post-page-author">By Stefan</span>
         <span className="blog-post-page-date">{date}</span>
       </div>
-      <div className="blog-post-page-content">
+        <img style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }} />
+      <div style={{ maxWidth: '800px', marginTop: '20px' }} className="blog-post-page-content">
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
       </div>
     </div>
