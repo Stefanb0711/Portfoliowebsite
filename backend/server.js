19911e4c270a5ebdbbab7d69874274c14196b82d
stefa <stefanboehme12@gmail.com>
2024-10-23 13:55:25 +0200

    ssl für postgres hinzugefügt

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const port = 5000;
 
 
 const db = new pg.Client({
-    connectionString: /*process.env.DATABASE_URL ||*/ 'postgresql://projects_6sy1_user:AlqfJmYpXArq6LBNYzBCERfGvEwxMOcS@dpg-cs87i8u8ii6s73c5f1lg-a.frankfurt-postgres.render.com/projects_6sy1'
+    connectionString: /*process.env.DATABASE_URL ||*/ 'postgresql://projects_6sy1_user:AlqfJmYpXArq6LBNYzBCERfGvEwxMOcS@dpg-cs87i8u8ii6s73c5f1lg-a.frankfurt-postgres.render.com/projects_6sy1',
+    ssl: {
+    rejectUnauthorized: false  // Unsigned Zertifikate akzeptieren, wenn nötig
+  }
 });
 
 // Mit der Datenbank verbinden
